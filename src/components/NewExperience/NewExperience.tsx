e126993dbca7e3cd7203c55772f1d0f26ab38240
David Qing <davidqing6432@gmail.com>
2025-01-09 12:35:27 -0800

    streamlined forms for experience submission

--- src/components/NewExperience/NewExperience.tsx
+++ src/components/NewExperience/NewExperience.tsx
@@ -4,52 +4,30 @@ import styles from "./NewExperience.module.css";
 import { createExperience } from "@/utils/experience/actions";
 import { useActionState, useState } from "react";
 
-const initialState = {
-  error: null,
-  numInfos: 0,
-}
-
 export default function NewExperience() {
-  const [numInfos, setNumInfos] = useState(initialState.numInfos);
-  const [state, formAction] = useActionState(createExperience, initialState);
-  
+  const [numInfos, setNumInfos] = useState(0);
+  const [state, formAction] = useActionState(createExperience, { error: null });
+
   return (
     <form
       className={styles.container}
       action={(formData) => {
         formAction(formData);
-        setNumInfos(state.numInfos);
+        // TODO: if the result of the form action is an error, we don't want to reset to 0, instead, maintain data.
+        setNumInfos(0);
       }}
     >
       <h2>New Experience</h2>
       <label htmlFor="organization">Organization</label>
-      <input
-        type="text"
-        name="organization"
-        placeholder="Organization"
-      />
+      <input type="text" name="organization" placeholder="Organization" />
       <label htmlFor="role">Role</label>
-      <input
-        type="text"
-        name="role"
-        placeholder="Role"
-      />
+      <input type="text" name="role" placeholder="Role" />
       <label htmlFor="start_date">Start Date</label>
-      <input
-        type="date"
-        name="start_date"
-      />
+      <input type="date" name="start_date" />
       <label htmlFor="end_date">End Date</label>
-      <input
-        type="date"
-        name="end_date"
-      />
+      <input type="date" name="end_date" />
       <label htmlFor="location">Location</label>
-      <input
-        type="text"
-        name="location"
-        placeholder="Location"
-      />
+      <input type="text" name="location" placeholder="Location" />
       <label htmlFor="information">Information</label>
       <div className={styles.multiInput}>
         {Array.from(Array(numInfos).keys()).map((index) => (
@@ -64,7 +42,6 @@ export default function NewExperience() {
           type="button"
           onClick={() => {
             setNumInfos(numInfos + 1);
-            state.numInfos += 1;
           }}
         >
           Add Information
--- src/utils/experience/actions.ts
+++ src/utils/experience/actions.ts
@@ -20,44 +20,41 @@ export async function fetchUserExperiences(): Promise<Experience[]> {
   return databaseExperiences.map((experience) => databaseToExperience(experience));
 }
 
-// export async function fetchExperiencesByUser(user_id: UUID): Promise<Experience[]> {
-//   const supabase = await createClient();
-//   const { data: experiences } = await supabase.from("experiences").select().eq("user_id", user_id);
-//   const databaseExperiences = experiences as DatabaseExperience[];
-//   return databaseExperiences.map((experience) => databaseToExperience(experience));
-// }
-
 type FormState = {
   error: string | null;
-  numInfos: number;
 }
 
 export async function createExperience(prevState: FormState, formData: FormData): Promise<FormState> {
   const supabase = await createClient();
   const information: string[] = [];
-  for (let i = 0; i < prevState.numInfos; i++) {
-    information.push(formData.get(`information_${i}`) as string);
+  let infoIndex = 0;
+  while (formData.get(`information_${infoIndex}`) as string) {
+    const info = formData.get(`information_${infoIndex}`) as string;
+    if (info) information.push(info);
+    infoIndex++;
   }
+  
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) {
-    return { error: "User not found", numInfos: prevState.numInfos };
+    return { error: "User not found" };
   }
   const userId = user.id as UUID;
   const experience: DatabaseExperience = {
     user_id: userId,
-    organization: formData.get("organization")?.toString()!,
-    role: formData.get("role")?.toString()!,
-    start_date: formData.get("start_date")?.toString()!,
-    end_date: formData.get("end_date")?.toString()!,
-    location: formData.get("location")?.toString()!,
+    organization: formData.get("organization")!.toString(),
+    role: formData.get("role")!.toString(),
+    start_date: formData.get("start_date")!.toString(),
+    end_date: formData.get("end_date")!.toString(),
+    location: formData.get("location")!.toString(),
     information: information,
   }
+
   const { data, error } = await supabase.from("experiences").insert(experience);
   if (error) {
     console.error(error);
-    return { error: error.message, numInfos: prevState.numInfos };
+    return { error: error.message };
   }
   // TODO: Check this path
   revalidatePath("/experiences");
-  return { error: null, numInfos: 0 };
+  return { error: null };
 }
